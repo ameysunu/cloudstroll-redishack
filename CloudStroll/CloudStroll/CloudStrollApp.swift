5c89858c87969309050e7adff6bf98352af0d96f
ameysunu <ameysunu00719@gmail.com>
2025-07-31 21:00:36 +0100

    swift app added with sign in with apple

---
+++ CloudStroll/CloudStroll/CloudStrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudStrollApp.swift
+//  CloudStroll
+//
+//  Created by Amey Sunu on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CloudStrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
